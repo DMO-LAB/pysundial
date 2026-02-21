46e13277e9d207bd16c4b618ad7f2beda1e71255
elotech47 <eloghosaefficiency@gmail.com>
2026-02-20 19:15:02 -0600

    Update CMake configuration, add LICENSE file, and enhance CI workflows
    - Updated CMakeLists.txt to provide clearer error messages for missing SUNDIALS headers.
    - Added LICENSE file with MIT License details.
    - Modified MANIFEST.in to include LICENSE and README.md.
    - Updated pyproject.toml to specify the license and improve metadata.
    - Enhanced CI workflows to support additional Python versions and install SUNDIALS dependencies.
    - Improved publish workflow to build and publish both source distributions and wheels to PyPI.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 import os
 import sys
 import subprocess
 import sysconfig
 
+
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
         super().__init__(name, sources=[])
         self.sourcedir = os.path.abspath(sourcedir)
+
 
 class CMakeBuild(build_ext):
     def run(self):
@@ -27,7 +29,10 @@ class CMakeBuild(build_ext):
         cfg = 'Debug' if self.debug else 'Release'
 
         python_include_dir = sysconfig.get_paths()['include']
-        python_library = os.path.join(sysconfig.get_config_var('LIBDIR'), 'libpython' + sysconfig.get_python_version() + '.so')
+        python_library = os.path.join(
+            sysconfig.get_config_var('LIBDIR') or '',
+            'libpython' + sysconfig.get_python_version() + '.so'
+        )
 
         cmake_args = [
             f'-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}',
@@ -35,7 +40,7 @@ class CMakeBuild(build_ext):
             f'-DPYTHON_EXECUTABLE={sys.executable}',
             f'-DPython_EXECUTABLE={sys.executable}',
             f'-DPYTHON_INCLUDE_DIR={python_include_dir}',
-            f'-DPYTHON_LIBRARY={python_library}'
+            f'-DPYTHON_LIBRARY={python_library}',
         ]
 
         build_args = ['--config', cfg, '--', '-j4']
@@ -49,25 +54,15 @@ class CMakeBuild(build_ext):
         build_temp = os.path.join(self.build_temp, ext.name)
         os.makedirs(build_temp, exist_ok=True)
 
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=build_temp)
+        subprocess.check_call(
+            ['cmake', ext.sourcedir] + cmake_args, cwd=build_temp, env=env
+        )
+        subprocess.check_call(
+            ['cmake', '--build', '.'] + build_args, cwd=build_temp
+        )
+
 
 setup(
-    name='SundialsPy',
-    version='0.1.0',
-    author='Eloghosa Ikponmwoba',  
-    author_email='eloghosaefficiency@gmail.com', 
-    description='Python bindings for SUNDIALS', 
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    packages=find_packages(where='.'),
-    package_dir={'SundialsPy': 'SundialsPy'},
     ext_modules=[CMakeExtension('SundialsPy._SundialsPy')],
-    cmdclass=dict(build_ext=CMakeBuild),
-    zip_safe=False,
-    install_requires=[
-        'numpy',
-        # Add any other Python dependencies here
-        ],
-    python_requires='>=3.7', # Or your minimum Python version
+    cmdclass={'build_ext': CMakeBuild},
 )
